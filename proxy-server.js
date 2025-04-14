f2ff7eb091f09c1489e4e3e81db7bd9e362aeacc
Perry Johnson <92198920+PerryJohnsonUC@users.noreply.github.com>
2025-04-14 19:49:32 -0400

    Update proxy-server.js

--- proxy-server.js
+++ proxy-server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const BEARER_TOKEN = 'Bearer eyJhbGciOiJIUzI1...'; // <== paste your real token here
+const BEARER_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2FwaTIuYXJkdWluby5jYy9pb3QiLCJhenAiOiJvV2ZHTVFKV2JteWE4NGF1TnB1SHJ1RHVBVXFMeXYyciIsImV4cCI6MTc0NDY3NDg0NCwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIiwiaHR0cDovL2FyZHVpbm8uY2MvY2xpZW50X2lkIjoiZ3JlZW4tdG9vbGtpdC1rZXkiLCJodHRwOi8vYXJkdWluby5jYy9pZCI6IjZkOGY1Yzk1LTBlY2QtNDQyNi05NTg3LTY4ZWI1NTdkMDYyYiIsImh0dHA6Ly9hcmR1aW5vLmNjL3JhdGVsaW1pdCI6MTAsImh0dHA6Ly9hcmR1aW5vLmNjL3VzZXJuYW1lIjoibW9udG95bWwiLCJpYXQiOjE3NDQ2NzQ1NDQsInN1YiI6Im9XZkdNUUpXYm15YTg0YXVOcHVIcnVEdUFVcUx5djJyQGNsaWVudHMifQ.Pn0QPPq-irCnBDLEbTQyTtlV7SY93s7V_h6lb0R3OA4'; // <== paste your real token here
 
 app.use(express.json());
 
